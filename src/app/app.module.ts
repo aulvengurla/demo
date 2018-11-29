5e1b8bafc892deba4f7cdd8786c8f829c136daf2
K <atul.k@capgemini.com>
2018-11-29 23:09:20 +0530

    update-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { AdminModule } from './admin/admin.module';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -9,14 +8,7 @@ import { StarterHeaderComponent } from './starter/starter-header/starter-header.
 import { StarterLeftSideComponent } from './starter/starter-left-side/starter-left-side.component';
 import { StarterContentComponent } from './starter/starter-content/starter-content.component';
 import { StarterFooterComponent } from './starter/starter-footer/starter-footer.component';
-import { StarterControlSidebarComponent } from './starter/starter-control-sidebar/starter-control-sidebar.component';
-import { AdminComponent } from './admin/admin.component';
-import { AdminHeaderComponent } from './admin/admin-header/admin-header.component';
-import { AdminLeftSideComponent } from './admin/admin-left-side/admin-left-side.component';
-import { AdminContentComponent } from './admin/admin-content/admin-content.component';
-import { AdminFooterComponent } from './admin/admin-footer/admin-footer.component';
-import { AdminControlSidebarComponent } from './admin/admin-control-sidebar/admin-control-sidebar.component';
-import { AdminDashboard1Component } from './admin/admin-dashboard1/admin-dashboard1.component';
+import { StarterControlSidebarComponent } from './starter/starter-control-sidebar/starter-control-sidebar.component'; 
 
 @NgModule({
   declarations: [
@@ -30,8 +22,7 @@ import { AdminDashboard1Component } from './admin/admin-dashboard1/admin-dashboa
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    AdminModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
