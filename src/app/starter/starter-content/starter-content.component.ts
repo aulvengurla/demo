c788391396c337c207fda7c8cfaabaa18e25f65a
K <atul.k@capgemini.com>
2019-01-16 14:42:22 +0530

    update16

--- src/app/starter/starter-content/starter-content.component.ts
+++ src/app/starter/starter-content/starter-content.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit,HostListener  } from '@angular/core';
+import { Component, OnInit,HostListener, ANALYZE_FOR_ENTRY_COMPONENTS  } from '@angular/core';
 import { MessageService } from '../../services/message.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+declare var $: any;
 
 @Component({
   selector: 'app-starter-content',
@@ -22,16 +24,13 @@ export class StarterContentComponent implements OnInit {
   inputString = "ipt_";
   updateMsg : any;
   display='none';
+   
+  bkData:string;
 
   ngOnInit() { 
       this.getAllMessage(); 
   }
-
-  getOrignalVal(data:any,id,i){ 
-     this.originalVal = data;
-     this.msgInputId = id;
-     this.updateMsg = i;
-  }
+ 
   @HostListener('document:keydown', ['$event']) onKeydownHandler(event: KeyboardEvent) {
     if (event.key === "Escape") {  
       this.messageList[this.updateMsg].msg = this.originalVal;
@@ -39,30 +38,59 @@ export class StarterContentComponent implements OnInit {
   }
 
   lenghtValidate(data:any){ 
-    console.log(data.length);
+    console.log(data);
     if(data.length >=75){
       alert("Message Should Not Be More Than 75 Characters.");
       return false;
     }
     
     return true;
-  }
- 
+  }  
+
   getAllMessage(){
     this.messageService.getAllMessage().subscribe((messageList) => { 
       this.messageList = messageList;
+      this.bkData = JSON.stringify(messageList); 
       console.log(this.messageList);
     }, err  => this.errorMsg = <any>err);
   }
 
   
-  updateMessage(data:any){ 
+  updateMessage(data:any,i){ 
+    this.errorMsg = '';
+    let bkDataObj = JSON.parse(this.bkData); 
+    let updatedStr = data.msg.replace(/ /g, "");
+    console.log(updatedStr);
+
+    $("#info_"+data.msgID).hide();
+    $("#save_"+data.msgID).hide();
+    $("#error_"+data.msgID).hide();
+
+    if(bkDataObj[i].msg == updatedStr){   
+     $("#info_"+data.msgID).show();
+    }else{        
+      this.messageService.updateMessage(data).subscribe((result) => {        
+        console.log(result);
+        this.getAllMessage();
+        $("#info_"+data.msgID).hide();
+        $("#save_"+data.msgID).show();
+        $("#error_"+data.msgID).hide();
+
+      }, err  => this.errorMsg = <any>err); 
+
+      console.log(this.errorMsg);
+
+      if(this.errorMsg){
+        $("#info_"+data.msgID).hide();
+        $("#save_"+data.msgID).hide();
+        $("#error_"+data.msgID).show();
+      }
  
-     console.log(data);
-     this.messageService.updateMessage(data).subscribe((result) => {        
-      console.log(result);
-      this.getAllMessage();
-    }, err  => this.errorMsg = <any>err); 
+    }
+
+    
+     
+
   }
 
   persistMsgTmp(data:any){
